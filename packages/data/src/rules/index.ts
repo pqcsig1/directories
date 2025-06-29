a5bc8b509bf52dac268595f6685db4c4c1f6d063
Nitin Gupta <gniting@users.noreply.github.com>
2025-06-29 11:59:10 +0200

    Updated to reference Prisma ruleset

--- packages/data/src/rules/index.ts
+++ packages/data/src/rules/index.ts
@@ -48,6 +48,7 @@ import { onchainkitRules } from "./onchainkit";
 import { openApiUserStoryRules } from "./open-api-user-story";
 import { pixiJsRules } from "./pixijs";
 import { playwrightRules } from "./playwright";
+import { prismaRules } from "./rules/prisma";
 import { pythonRules } from "./python";
 import { railsRules } from "./rails";
 import { reactNativeRules } from "./react-native";
@@ -126,6 +127,7 @@ export const rules: Rule[] = [
   ...openApiUserStoryRules,
   ...pixiJsRules,
   ...playwrightRules,
+  ...prismaRules,
   ...pythonRules,
   ...railsRules,
   ...reactNativeRules,
