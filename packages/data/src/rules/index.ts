11fcaec9454b90b5e1e21b71783259d71ced78f6
Md. Morshadun Nur <morshadunnur@gmail.com>
2025-07-21 13:17:22 +0000

    Add rules for Shopify Theme development rules

--- packages/data/src/rules/index.ts
+++ packages/data/src/rules/index.ts
@@ -77,6 +77,7 @@ import { webDevelopmentRules } from "./web-development";
 import { webScrapingRules } from "./web-scraping";
 import { wordpressRules } from "./wordpress";
 import { wordpressWoocommerce } from "./wordpress-woocommerce";
+import { shopifyThemeRules } from "./shopify-theme-development";
 
 export const rules: Rule[] = [
   ...ALRules,
@@ -157,6 +158,7 @@ export const rules: Rule[] = [
   ...wordpressWoocommerce,
   ...typescriptRules,
   ...viewComfyRules,
+  ...shopifyThemeRules,
 ].map(
   (rule): Rule => ({
     ...rule,
